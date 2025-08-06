d2e575d8b934bac0aee166c94718bfb473e8d9f1
= <=>
2025-08-06 13:09:34 +0530

    Login Screen In Progress

--- lib/features/auth/presentation/pages/login_screen.dart
+++ lib/features/auth/presentation/pages/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:institute_attendance_system/features/auth/presentation/widgets/auth_text_field.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -8,46 +9,61 @@ class LoginScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
           backgroundColor: Colors.black87,
-          body: Center(
-            child: Column(
-              children: [
-                Row(
+          body: Column(
+            children: [
+              SizedBox(height: 5),
+              SizedBox(
+                height: 25,
+                child: Row(
                   children: [
-                    SizedBox(
-                      width: 25,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 5.0),
-                      child: Icon(
-                        Icons.school,
-                        color: Colors.white,
-                      ),
-                    ),
                     SizedBox(
                       width: 10,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 4.0),
-                      child: Text(
-                        "Smart Attendance System",
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w300,
-                            color: Colors.white),
-                      ),
+                    Text(
+                      "Smart Attendance System (Admin)",
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: const Color.fromARGB(255, 142, 194, 234)),
                     )
                   ],
                 ),
-                Divider(
-                  color: const Color.fromARGB(255, 62, 62, 62),
+              ),
+              Divider(
+                color: const Color.fromARGB(255, 48, 48, 48),
+              ),
+              Expanded(
+                child: SizedBox(
+                  child: Center(
+                      child: SizedBox(
+                    height: 400,
+                    width: 600,
+                    child: Card(
+                      elevation: 5,
+                      color: const Color.fromARGB(255, 31, 31, 31),
+                      child: Column(
+                        children: [
+                          SizedBox(height: 20),
+                          Text(
+                            "Welcome Back",
+                            style: TextStyle(color: Colors.white, fontSize: 30),
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            "Login",
+                            style: TextStyle(color: Colors.white, fontSize: 28),
+                          ),
+                          AuthTextField(
+                            label: "Email",
+                            initialValue: "Enter Email",
+                            onChanged: (p0) {},
+                          )
+                        ],
+                      ),
+                    ),
+                  )),
                 ),
-                Container(
-                  width: 500,
-                  height: 200,
-                  color: Colors.red,
-                )
-              ],
-            ),
+              )
+            ],
           )),
     );
   }
